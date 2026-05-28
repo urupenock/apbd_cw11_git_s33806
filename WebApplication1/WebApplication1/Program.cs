cf6edbbec2f320f50f0e4153a11d0540cadf1ecb
anmrew1 <urga068@gmail.com>
2026-05-28 23:53:40 +0200

    add srtart point

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -4,8 +4,11 @@ using WebApplication1.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+    });
 
 builder.Services.AddOpenApi();
 
@@ -14,17 +17,26 @@ builder.Services.AddDbContext<HospitalContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+
 builder.Services.AddScoped<IHospitalService, HospitalService>();
 
 var app = builder.Build();
 
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi(); 
+    
+  
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Hospital API V1");
+    });
 }
 
 app.UseAuthorization();
-
 app.MapControllers();
+app.MapGet("/", async context => context.Response.Redirect("/swagger"));
 
 app.Run();
